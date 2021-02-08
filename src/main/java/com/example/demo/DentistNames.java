4c09c4fa37b96c296b2e58b93cdc592a24ab933f
Aivar Apsalon <apsalona@gmail.com>
2021-02-08 18:54:43 +0200

    Initial commit

---
+++ src/main/java/com/example/demo/DentistNames.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+public class DentistNames {
+    private String dentistName;
+    private String dentistId;
+
+    public String getDentistId() {
+        return dentistId;
+    }
+
+    public void setDentistId(String dentistId) {
+        this.dentistId = dentistId;
+    }
+
+    public String getDentistName() {
+        return dentistName;
+    }
+
+    public void setDentistName(String dentistName) {
+        this.dentistName = dentistName;
+    }
+}
